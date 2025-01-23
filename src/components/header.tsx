ae17931e3fcfdb15d3c009a58612b6781fa32603
robinmonsere <monsere.robin@gmail.com>
2025-01-23 22:58:12 +0100

    fix header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <header className="sticky top-0 bg-transparent pt-3">
+    <header className="md:sticky top-0 bg-transparent pt-3 mx-4">
         <div className={"max-w-5xl mx-auto flex flex-row gap-3 justify-end font-semibold"}>
             <Link href="/features">Features</Link>
             <Link href="#contact">Contact</Link>
